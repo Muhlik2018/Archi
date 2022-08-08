508436b603ef177c0b91ad1fa253cf3115cde6d3
Rylwl <928736874@qq.com>
2022-08-08 15:48:53 +0800

    写完首页去ToGenerate部分

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <HomePage></HomePage>
+</template>
+
+<script>
+import HomePage from "./components/HomePage.vue";
+export default {
+  name: "App",
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+#app {
+  height: 100%;
+  min-width: 1600px;
+  min-height: 900px;
+  max-width: 2560px;
+  max-height: 1440px;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="HomePage">
+    <div class="to-generate">
+      <div class="to-generate-footer">
+        <div class="tgf-left">
+          <div class="tgf-left-up">
+            <div>生成艺术</div>
+            <div>GENERATE ART</div>
+          </div>
+          <div class="tgf-left-down">
+            The quick brown fox jumps over the lazy dog
+          </div>
+        </div>
+        <div class="tgf-right">
+          <button class="tgf-right-button">去创作</button>
+        </div>
+      </div>
+      <el-image src="1.svg" fit="contain" style="width: 100%"></el-image>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style scoped>
+.HomePage {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.to-generate {
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+  position: relative;
+}
+.to-generate-footer {
+  position: absolute;
+  width: 100%;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.tgf-left{
+  width: 40rem;
+  margin: 10rem;
+}
+.tgf-left-up {
+  font-family: "Abel";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 5rem;
+  /* line-height: 7.625rem; */
+  color: #ffffff;
+}
+.tgf-left-down {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 44px;
+  text-transform: uppercase;
+  color: #ffffff;
+}
+.tgf-right-button {
+  width: 20rem;
+  height: 6rem;
+  background: #faed00;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  text-transform: uppercase;
+  color: #000000;
+  border: none;
+  margin: 10rem;
+}
+</style>
