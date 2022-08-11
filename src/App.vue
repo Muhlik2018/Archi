736b4c8c486e82173673e3c1383a53441adc4571
SayQuit <501241011@qq.com>
2022-08-11 20:56:47 +0800

    20220811 Achi members

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,26 @@
   这里要加overflow-x: hidden因为要隐藏浏览器和移动端的左右移动框
  -->
     <!-- <HeadNav></HeadNav> -->
-    <CulturalCreation></CulturalCreation>
+    <!-- <CulturalCreation></CulturalCreation> -->
     <!-- <FooterNav></FooterNav> -->
+    <TeamMembers></TeamMembers>
   </div>
 </template>
 
 <script>
-import CulturalCreation from "./components/CulturalCreation.vue";
+// import CulturalCreation from "./components/CulturalCreation.vue";
 // import FooterNav from "./components/FooterNav.vue";
 // import HeadNav from "./components/HeadNav.vue";
+import TeamMembers from "./components/TeamMembers.vue";
 import "./js/flexible.js";
 export default {
   name: "App",
   components: {
-    CulturalCreation,
+    // CulturalCreation,
     // FooterNav,
-    // HeadNav
-  },
+    // HeadNav,
+    TeamMembers
+},
 };
 </script>
 
