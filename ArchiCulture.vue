f265b80a5c269b3f118f4c3b31f45b83fe20334f
Yusi Zhang <84915379+zaozao1127@users.noreply.github.com>
2022-09-09 22:33:56 +0800

    修改了轮播图

---
+++ ArchiCulture.vue
@@ -0,0 +1,400 @@
+<template>
+  <div class="Page">
+    <HeadNav></HeadNav>
+    <div v-if="homeScene.length > 0" class="carousel">
+      <el-carousel height="61.875em" :autoplay="false" @change="turnPage">
+        <el-carousel-item v-for="(item, index) in homeScene" :key="index">
+          <div class="carousel-box">
+            <div class="carousel-text-box">
+              <div class="carousel-text">
+                <div class="carousel-text-title">{{ item.scene }}</div>
+                <!-- <div class="carousel-text-details">
+                  岭南文化作为中华民族传统文化中最具特色和活力的地域文化之一
+                </div> -->
+              </div>
+            </div>
+            <el-image :src="item.url" fit="contain" style="width: 100%">
+            </el-image>
+          </div>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div v-if="show == 0">
+      <WaterFall ref="freedomWaterFall" :list="freedom_list" />
+    </div>
+    <div v-if="show == 1">
+      <WaterFall ref="freedomWaterFall" :list="grid_list" />
+    </div>
+    <div v-if="show == 2">
+      <WaterFall ref="freedomWaterFall" :list="raster_list" />
+    </div>
+    <div v-if="show == 3">
+      <WaterFall4 ref="freedomWaterFall" :list="sketch_list" />
+    </div>
+    <div v-if="show == 4">
+      <WaterFall ref="freedomWaterFall" :list="fractal_list" />
+    </div>
+    <FooterNav></FooterNav>
+  </div>
+</template>
+
+<script>
+import HeadNav from "./HeadNav.vue";
+import FooterNav from "./FooterNav.vue";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import { useRouter } from "vue-router";
+import WaterFall from "./WaterFall.vue";
+import axios from "axios";
+import { reactive, ref } from "@vue/reactivity";
+import { onBeforeMount } from "@vue/runtime-core";
+import WaterFall4 from "./WaterFall4.vue";
+
+export default {
+  components: {
+    HeadNav,
+    FooterNav,
+    WaterFall,
+    WaterFall4,
+  },
+  data() {
+    return {
+      //   raster_list: [],
+      //   freedom_list: [],
+      //   grid_list: [],
+      //   sketch_list: [],
+    };
+  },
+  created() {
+    this.getPic();
+  },
+  setup() {
+    const router = useRouter();
+    const scene = ref(router.currentRoute.value.params.scene);
+    let show = ref(0);
+    let freedom_list = reactive([]);
+    let grid_list = reactive([]);
+    let sketch_list = reactive([]);
+    let fractal_list = reactive([]);
+    let raster_list = reactive([]);
+    // console.log("scene.value", scene.value);
+    if (scene.value === "freedom") {
+      console.log(0);
+      show.value = 0;
+    } else if (scene.value === "grid") {
+      console.log(1);
+      show.value = 1;
+    } else if (scene.value === "raster") {
+      console.log(2);
+      show.value = 2;
+    } else if (scene.value === "sketch") {
+      console.log(3);
+      show.value = 3;
+    } else if (scene.value === "fractal") {
+      console.log(4);
+      show.value = 4;
+    } else {
+      console.log(0);
+      show.value = 0;
+    }
+
+    let homeScene = reactive([]);
+
+    let getPic = () => {
+      axios({
+        method: "get",
+        url: "/ac/api/archicasca/freedom",
+        headers: {
+          "Content-Type": "x-www-form-urlencoded",
+        },
+      })
+        .then((res) => {
+          //   console.log("pic data", res);
+          if (res.data.code === 200) {
+            freedom_list.push(...res.data.data);
+            // console.log(freedom_list);
+            // console.log("freedom");
+          }
+        })
+        .catch((err) => {
+          console.log("err", err);
+        })
+        .finally(() => {
+          if (scene.value === "freedom") {
+            show.value=-1;
+            setTimeout(() => {
+              show.value=0;
+            }, 20);
+          }
+        });
+
+      axios({
+        method: "get",
+        url: "/ac/api/archicasca/grid",
+        headers: {
+          "Content-Type": "x-www-form-urlencoded",
+        },
+      })
+        .then((res) => {
+          console.log("pic data", res);
+          if (res.data.code === 200) {
+            res.data.data.forEach((element) => {
+              if (element.type === "svg") {
+                element.url = element.url.replace(".svg", ".png");
+              }
+            });
+            grid_list.push(...res.data.data);
+            // console.log(grid_list);
+            // console.log("grid");
+          }
+        })
+        .catch((err) => {
+          console.log("err", err);
+        })
+        .finally(() => {
+          if (scene.value === "grid") {
+            show.value=-1;
+            setTimeout(() => {
+              show.value=1;
+            }, 20);
+          }
+        });
+
+      axios({
+        method: "get",
+        url: "/ac/api/archicasca/sketch",
+        headers: {
+          "Content-Type": "x-www-form-urlencoded",
+        },
+      })
+        .then((res) => {
+          //   console.log("pic data", res);
+          if (res.data.code === 200) {
+            res.data.data.forEach((element) => {
+              if (element.type === "svg") {
+                element.url = element.url.replace(".svg", ".png");
+              }
+            });
+            sketch_list.push(...res.data.data);
+            // console.log(sketch_list);
+            // console.log("sketch");
+          }
+        })
+        .catch((err) => {
+          console.log("err", err);
+        })
+        .finally(() => {
+          if (scene.value === "sketch") {
+            show.value=-1;
+            setTimeout(() => {
+              show.value=3;
+            }, 20);
+          }
+        });
+
+      axios({
+        method: "get",
+        url: "/ac/api/archicasca/fractal",
+        headers: {
+          "Content-Type": "x-www-form-urlencoded",
+        },
+      })
+        .then((res) => {
+          //   console.log("pic data", res);
+          if (res.data.code === 200) {
+            res.data.data.forEach((element) => {
+              if (element.type === "svg") {
+                element.url = element.url.replace(".svg", ".png");
+              }
+            });
+            fractal_list.push(...res.data.data);
+            // console.log(fractal_list);
+            // console.log("fractal");
+          }
+        })
+        .catch((err) => {
+          console.log("err", err);
+        })
+        .finally(() => {
+          if (scene.value === "fractal") {
+            show.value=-1;
+            setTimeout(() => {
+              show.value=4;
+            }, 20);
+          }
+        });
+
+      axios({
+        method: "get",
+        url: "/ac/api/archicasca/raster",
+        headers: {
+          "Content-Type": "x-www-form-urlencoded",
+        },
+      })
+        .then((res) => {
+          //   console.log("pic data", res);
+          if (res.data.code === 200) {
+            res.data.data.forEach((element) => {
+              if (element.type === "svg") {
+                element.url = element.url.replace(".svg", ".png");
+              }
+            });
+            raster_list.push(...res.data.data);
+            // console.log(raster_list);
+            // console.log("fraster");
+          }
+        })
+        .catch((err) => {
+          console.log("err", err);
+        })
+        .finally(() => {
+          if (scene.value === "raster") {
+            show.value=-1;
+            setTimeout(() => {
+              show.value=2;
+            }, 20);
+          }
+        });
+    };
+
+    onBeforeMount(() => {
+      // 获取场景风格轮播图
+      axios
+        .get("/ac/api/image/scene")
+        .then(({ data }) => {
+          //   console.log("scene data", data);
+          if (data.code === 200) {
+            data = data.data;
+            // homeScene.value = data;
+            homeScene.push(...data);
+          }
+        })
+        .catch((err) => {
+          console.log("err", err);
+        });
+
+      getPic();
+    });
+    return {
+      router,
+      modules: [Navigation],
+      homeScene,
+      show,
+      scene,
+      getPic,
+      freedom_list,
+      grid_list,
+      sketch_list,
+      fractal_list,
+      raster_list,
+    };
+  },
+  methods: {
+    turnPage(index) {
+      this.show = index;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.Page {
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+}
+
+.carousel {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.carousel-box {
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+  position: relative;
+}
+
+.carousel .el-carousel {
+  width: 100%;
+  margin: 3rem 1rem;
+}
+
+.carousel-text-box {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.carousel-text-box::before {
+  content: "";
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.7) 5.03%,
+    rgba(0, 0, 0, 0) 29.12%
+  );
+  transform: rotate(-180deg);
+}
+
+.carousel-image-mask {
+  position: absolute;
+  width: 100%;
+  z-index: 500;
+}
+
+.carousel >>> .el-carousel__arrow {
+  transform: scale(3);
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: #000000;
+  margin: 0 3rem;
+}
+
+.carousel-text {
+  position: absolute;
+  width: 46rem;
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  margin: 3rem;
+}
+
+.carousel-text-title {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 6rem;
+  line-height: 8rem;
+  text-transform: uppercase;
+  color: #ffffff;
+}
+
+.carousel-text-details {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.875rem;
+  line-height: 2rem;
+  text-transform: uppercase;
+  color: #c8c8c8;
+  margin-top: 1em;
+}
+</style>
+
+<style>
+.el-carousel {
+  width: 100%;
+}
+</style>
---
+++ HomePage.vue
@@ -0,0 +1,586 @@
+<template>
+  <div class="HomePage">
+    <HeadNav></HeadNav>
+    <div class="to-generate">
+      <div class="to-generate-footer">
+        <div class="tgf-left">
+          <div class="tgf-left-up">
+            <div>生成艺术</div>
+            <div>GENERATE ART</div>
+          </div>
+          <div class="tgf-left-down">
+            The quick brown fox jumps over the lazy dog
+          </div>
+        </div>
+        <div class="tgf-right">
+          <button class="tgf-right-button" @click="goGenerate()">去创作</button>
+        </div>
+      </div>
+      <el-image :src="homeTheme" fit="contain" style="width: 100%"></el-image>
+    </div>
+    <div class="carousel" v-if="homeScene.length">
+      <el-carousel height="62rem" :autoplay="false">
+        <el-carousel-item
+          v-for="(item, index) in homeScene"
+          :key="index"
+          @click="goScenePage(item.scene)"
+        >
+          <!-- <h3 class="small justify-center" text="2xl">{{ item }}</h3> -->
+          <div class="carousel-box">
+            <div class="carousel-text-box">
+              <div class="carousel-text-content">
+                <div class="carousel-text-title">{{ item.scene }}</div>
+                <div class="carousel-text-details">
+                  The quick brown fox jumps over the lazy dog
+                </div>
+              </div>
+            </div>
+            <el-image
+              :src="item.url"
+              fit="contain"
+              style="width: 100%"
+            ></el-image>
+            <!-- <el-image
+              class="carousel-image-mask"
+              src="Home2-1.svg"
+              fit="contain"
+              style="width: 100%"
+            ></el-image> -->
+          </div>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="top5">
+      <div class="top5-header">
+        <div class="top5-header-title-up">today's top picks</div>
+        <div class="top5-header-title-down">今日的TOP5生成艺术作品</div>
+      </div>
+      <!-- swiper实现 -->
+      <div class="top5-swiper">
+        <swiper
+          :centeredSlides="true"
+          :spaceBetween="-300"
+          :slidesPerView="3.8"
+          :initialSlide="4"
+        >
+          <swiper-slide
+            v-for="(item, index) in homeClassic"
+            data-index="index"
+            :key="index"
+            @click="goDetail(item.scene, item.url)"
+          >
+            <div class="swiper-item">
+              <div class="swiper-item-text">
+                <div class="swiper-item-category">{{ item.scene }}</div>
+                <div class="swiper-item-description">算法程序</div>
+              </div>
+              <el-image :src="item.url" fit="contain"></el-image>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+    <!-- 下面的本阶段隐藏 -->
+    <div class="category" v-if="false">
+      <div class="category-header">分类</div>
+      <div class="category-box">
+        <div
+          class="category-item"
+          v-for="item in categoryContext"
+          :key="item.id"
+        >
+          <div class="category-item-text">
+            <div class="category-item-title">{{ item.title }}</div>
+            <div class="category-item-detail">{{ item.detail }}</div>
+          </div>
+          <el-image
+            :src="'Home4-' + item.id + '.svg'"
+            fit="contain"
+            style="width: 100%"
+          ></el-image>
+        </div>
+      </div>
+    </div>
+    <div class="collection" v-if="false">
+      <div class="collection-header">你的收藏</div>
+      <div class="collection-box">
+        <swiper navigation :modules="modules" :slidesPerView="5">
+          <swiper-slide
+            v-for="(item, index) in 7"
+            data-index="index"
+            :key="index"
+          >
+            <div class="collection-swiper-item">
+              <el-image :src="'Home5-' + item + '.svg'" fit="cover"></el-image>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+    <FooterNav></FooterNav>
+  </div>
+</template>
+
+<script>
+import HeadNav from "./HeadNav.vue";
+import FooterNav from "./FooterNav.vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import { reactive, ref } from "@vue/reactivity";
+import { useRouter } from "vue-router";
+import { onBeforeMount } from "@vue/runtime-core";
+import axios from "axios";
+
+export default {
+  name: "HomePage",
+  components: {
+    Swiper,
+    SwiperSlide,
+    HeadNav,
+    FooterNav,
+  },
+  setup() {
+    const router = useRouter();
+    let categoryContext = reactive([
+      {
+        id: 1,
+        title: "地区",
+        detail: "在不同地区之间探索艺术作品",
+      },
+      {
+        id: 2,
+        title: "主题",
+        detail: "依据主题来开启艺术之旅",
+      },
+      {
+        id: 3,
+        title: "颜色",
+        detail: "根据颜色来发现艺术作品",
+      },
+    ]);
+    let homeTheme = ref("");
+    let homeClassic = reactive([]);
+    let homeScene = reactive([]);
+
+    onBeforeMount(() => {
+      // 获取主题背景图
+      axios
+        .get("/ac/api/image/theme")
+        .then(({ data }) => {
+          // console.log("theme data", data);
+          if (data.code === 200) {
+            data = data.data;
+            homeTheme.value = data.url;
+          }
+        })
+        .catch((err) => {
+          console.log("err", err);
+        });
+
+      // 获取经典画作轮播图
+      axios
+        .get("/ac/api/image/classic")
+        .then(({ data }) => {
+          console.log("classic data", data);
+          if (data.code === 200) {
+            data = data.data;
+            data.forEach((element) => {
+              const str = element.url.replace(".svg", ".png");
+              // console.log("str", str);
+              element.url = str;
+            });
+            homeClassic.push(...data);
+          }
+        })
+        .catch((err) => {
+          console.log("err", err);
+        });
+
+      // 获取场景风格轮播图
+      axios
+        .get("/ac/api/image/scene")
+        .then(({ data }) => {
+          // console.log("scene data", data);
+          if (data.code === 200) {
+            data = data.data;
+            homeScene.push(...data);
+          }
+        })
+        .catch((err) => {
+          console.log("err", err);
+        });
+    });
+
+    return {
+      categoryContext,
+      router,
+      modules: [Navigation],
+      homeTheme,
+      homeClassic,
+      homeScene,
+    };
+  },
+  methods: {
+    goDetail(scene, pngUrl) {
+      const url = pngUrl.replace(".png", ".svg");
+      this.router.push({ name: "detail", params: { scene, url } });
+    },
+    goGenerate() {
+      this.router.push({ name: "GenerateArt1" });
+    },
+    goScenePage(scene) {
+      if (scene) {
+        console.log("scene", scene);
+        this.router.push({ name: "ArchiCulture", params: { scene } });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.HomePage {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+}
+.to-generate {
+  width: 100%;
+  display: flex;
+  align-items: flex-end;
+  position: relative;
+}
+.to-generate-footer {
+  position: absolute;
+  width: 100%;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.to-generate::after {
+  content: "";
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.7) 0%,
+    rgba(0, 0, 0, 0) 58.35%
+  );
+  transform: rotate(-180deg);
+}
+.tgf-left {
+  width: 40rem;
+  margin: 4rem;
+}
+.tgf-left-up {
+  font-family: "Abel";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 5rem;
+  /* line-height: 7.625rem; */
+  color: #ffffff;
+}
+.tgf-left-down {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 44px;
+  text-transform: uppercase;
+  color: #ffffff;
+}
+.tgf-right-button {
+  width: 20rem;
+  height: 6rem;
+  background: #faed00;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  text-transform: uppercase;
+  color: #000000;
+  border: none;
+  margin: 4rem;
+}
+
+.carousel {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+}
+.carousel .el-carousel {
+  width: 100%;
+  margin: 3rem 1rem;
+}
+.carousel >>> .el-carousel__arrow {
+  transform: scale(3);
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: #000000;
+  margin: 0 3rem;
+}
+.carousel-box {
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+  position: relative;
+}
+.carousel-image-mask {
+  position: absolute;
+  width: 100%;
+  z-index: 500;
+}
+.carousel-text-box {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+.carousel-text-box::before {
+  content: "";
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.7) 5.03%,
+    rgba(0, 0, 0, 0) 29.12%
+  );
+  transform: rotate(-180deg);
+}
+.carousel-text-content {
+  width: 40rem;
+  position: absolute;
+  z-index: 500;
+  margin: 2rem 4rem;
+}
+.carousel-text-title {
+  font-family: "Arimo";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 3rem;
+  line-height: 3.5rem;
+  text-transform: uppercase;
+  color: #ffffff;
+}
+.carousel-text-details {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 2rem;
+  line-height: 2.5rem;
+  text-transform: uppercase;
+  color: #ffffff;
+}
+
+.top5 {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(
+    180deg,
+    #754abc 0%,
+    rgba(255, 255, 255, 0) 74.04%
+  );
+  margin: 1rem;
+}
+.top5-header {
+  margin-top: 5rem;
+  margin-bottom: 2rem;
+}
+.top5-header-title-up {
+  font-family: "Arimo";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 3rem;
+  line-height: 3rem;
+  text-align: center;
+  text-transform: uppercase;
+  color: #fafafa;
+}
+.top5-header-title-down {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 1.75rem;
+  line-height: 2.5rem;
+  text-align: center;
+  text-transform: uppercase;
+  color: #ffffff;
+}
+.top5-swiper {
+  width: 90%;
+  height: 40rem;
+  margin: auto;
+}
+.top5-swiper .swiper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  height: 100%;
+}
+.top5-swiper >>> .swiper-wrapper {
+  align-items: center;
+  justify-content: center;
+  margin-left: 75rem;
+  height: 100%;
+}
+.top5-swiper .swiper-slide {
+  text-align: center;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 300ms;
+  transform: scale(0.7);
+  margin-right: -14rem !important;
+}
+.top5-swiper .swiper-item {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+  position: relative;
+  height: 100%;
+}
+.top5-swiper .el-image {
+  height: 100%;
+}
+.top5-swiper .swiper-slide-active,
+.top5-swiper .swiper-slide-duplicate-active {
+  transform: scale(0.95);
+}
+.top5-swiper .swiper-slide-next {
+  transform: scale(0.8);
+  z-index: 998 !important;
+}
+.top5-swiper .swiper-slide-prev {
+  transform: scale(0.8);
+}
+.top5-swiper .swiper-slide-active {
+  z-index: 999 !important;
+}
+.top5-swiper .swiper-item-text {
+  position: absolute;
+  width: 100%;
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  color: #754abc;
+  z-index: 1000;
+  margin: 0 3rem;
+}
+.top5-swiper .swiper-item-category {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 2rem;
+  line-height: 2rem;
+  text-transform: uppercase;
+}
+.top5-swiper .swiper-item-description {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.3rem;
+  line-height: 1.5rem;
+  text-transform: uppercase;
+  margin-top: 0.5rem;
+}
+.top5-swiper >>> .el-image__inner {
+  background-color: white;
+}
+
+.category {
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 1rem;
+}
+.category-header,
+.collection-header {
+  margin-left: 1.5rem;
+  font-family: "Eczar";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 2.5rem;
+  line-height: 4rem;
+  color: #000000;
+}
+.category-box {
+  display: flex;
+  justify-content: space-around;
+}
+.category-item {
+  margin: 1.5rem;
+  display: flex;
+  align-items: flex-end;
+  position: relative;
+}
+.category-item-text {
+  position: absolute;
+  width: 100%;
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  color: #ffffff;
+  background: linear-gradient(
+    180deg,
+    rgba(217, 217, 217, 0) 0%,
+    rgba(0, 0, 0, 0.61) 82.03%
+  );
+}
+.category-item-title {
+  font-family: "Amaranth";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  letter-spacing: 0.02rem;
+  text-transform: uppercase;
+  margin-left: 1rem;
+  margin-bottom: 0.5rem;
+}
+.category-item-detail {
+  font-family: "Amaranth";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1rem;
+  line-height: 2rem;
+  letter-spacing: 0.01rem;
+  text-transform: uppercase;
+  color: #ffffff;
+  margin-left: 1rem;
+  margin-bottom: 1rem;
+}
+
+.collection {
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 1rem;
+}
+.collection .swiper-slide {
+  margin: 0.5rem;
+  display: flex;
+  justify-content: center;
+}
+</style>
